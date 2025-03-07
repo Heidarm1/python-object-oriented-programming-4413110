12bb73ed64f1ec1e70b056f71281278a6aa6820b
Heidarm1 <mehrshad.heidari.heidari@hva.nl>
2025-03-07 02:00:37 +0000

    Chapter 1 DONE

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class book:
+      def __init__(self, title): 
+        self.title = title
 
 # TODO: create instances of the class
-
+book1 = book("Brave New World")
+book2 = book("War and Peace")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
